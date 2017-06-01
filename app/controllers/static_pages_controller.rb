304bb5f0689bfaf3b61e8bc937c1e2d7ed17ee16
LGM1213 <lgm1213@gmail.com>
2017-06-01 19:31:40 -0400

    added static pages controller, added bcrypt gem, edited routes

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def About
+  end
+
+  def Contact
+  end
+
+  def Help
+  end
+
+  def Home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # static pages
+  root 'static_pages#Home'
+  get 'static_pages/About'
+  get 'static_pages/Contact'
+  get 'static_pages/Help'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
