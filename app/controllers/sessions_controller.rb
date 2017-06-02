fe0bd811193ae55ab97f41512da7cbb9665ac209
LGM1213 <lgm1213@gmail.com>
2017-06-02 11:36:52 -0400

    filled in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,30 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(username: params[:session][:username])
+  	if user && user.authenticate(params[:session][:password])
+  		if user.activated?
+        #log the user in and redirect to user's show page
+    		log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    		redirect_back_or user
+    	else
+        message = "Account not activated."
+        message = "Check your email for the activation link."
+    		#creates an error message
+  	  	flash[:warning] = message
+  	  	redirect_to root_url
+  	  end
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  	log_out if logged_in?
+  	redirect_to root_url
+  end
 end
