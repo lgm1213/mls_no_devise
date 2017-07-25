f37f0b4789a2f3557bfb6cd0c614bb138c124a18
LGM1213 <lgm1213@gmail.com>
2017-07-24 22:44:03 -0400

    edited listing views and udpated model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,8 @@
 class Listing < ApplicationRecord
   belongs_to :building
+  belongs_to :user
 
   has_many :listing_interior_features
   has_many :interior_features, :through => :listing_interior_features
+  
 end
