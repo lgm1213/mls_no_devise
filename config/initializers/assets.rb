72249de10c786a013b883faa9f0d8a3b10699774
lgm1213 <lgm1213@gmail.com>
2017-08-25 10:21:08 -0400

    precompiled and updated asset path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.paths << Rails.root.join("vendor", "assets")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 
 
 # Precompile additional assets.
