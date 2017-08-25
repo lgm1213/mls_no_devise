882280e302a4fb1e93bb026306538932c0c75479
lgm1213 <lgm1213@gmail.com>
2017-08-25 10:37:41 -0400

    bring back asset path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
-# Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
-# Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
 
 
 # Precompile additional assets.
