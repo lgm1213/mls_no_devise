89aa62973d6d6988e40666ed18074cdbbdb9c122
lgm1213 <lgm1213@gmail.com>
2017-09-02 13:17:36 -0400

    debugger method added to sessions controller as a test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(username: params[:session][:username])
+    logger.debug user
   	if user && user.authenticate(params[:session][:password])
   		if user.activated?
         puts "User Activated"
