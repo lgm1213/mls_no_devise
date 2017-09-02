9941798831b883a97f80e316d3bf97c02688cceb
lgm1213 <lgm1213@gmail.com>
2017-09-02 14:08:19 -0400

    added user by seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,54 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Building.create!(county:'Test ',
-area: 'Test ',
-city:'Test ', 
-folio:'Test ',
-street:'Test ',
-compass_point:'Test ',
-street_name:'Test ',
-state:'Test ',
-zip:'Test ',
-zip4:'Test ',
-unit:'Test ',
-legal:'Test ',
-zoning:'Test ',
-geographical:'Test ',
-municip_code:'Test ',
-township:'Test ',
-section:'Test ',
-subdivision:'Test ',
-parcel:'Test ',
-map_coordinates:'Test ',
-elementary_school:'Test ',
-middle_school:'Test ',
-senior_high_school:'Test ',
-subdivision_name:'Test ',
-development_name:'Test ',
-model_name_in_mls:'Test ',
-user_id: 1,
-created_at:Time.zone.now,
-updated_at:Time.zone.now,
-year_built:'Test ',
-year_built_description:'Test ',
-stories:'Test ',
-convert_beds:'Test ',
-garage_spaces:'Test ',
-garage_description:'Test ',
-carport_spaces:'Test ',
-carport_description:'Test ',
-waterfront: true,
-waterfront_frontage: 'Test ',
-approx_lot_size:'Test ',
-lotsf:'Test ',
-pool: true,
-pool_dimensions:'Test ',
-spa: false,
-lot_description:'Test ',
-waterfront_description:'Test',
-parking_restrictions: 'Test'
+User.create!( username:'testuser1 ',
+	email: 'test1@test.com',
+	password: 'password',
+	password_confirmation: 'password',
+	activated: true,
+	admin: true
+	)
 
-)
-
-p "created building"
+p "created user 1"
